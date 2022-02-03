8f50196e91156fdbf055a45ef3ed1dda64c5ecec
laerte.guimaraes <laerte.guimaraes@live.com>
2022-02-03 19:51:58 -0300

    Realiza ajustes para execução do projeto

--- spec/factories/rentals.rb
+++ spec/factories/rentals.rb
@@ -4,10 +4,16 @@ FactoryBot.define do
   factory :rental do
     start_date { '2019-11-13' }
     end_date { '2019-11-13' }
-    client { nil }
-    category { nil }
-    subsidiary { nil }
+    client
+    category
+    subsidiary
+    price_projection { 100.0 }
     status { 0 }
+
+    before(:create) do |rental|
+      car_model = create(:car_model, category: rental.category)
+      create(:car, car_model: car_model)
+    end
 
     trait :without_callbacks do
       after(:build) do |rental|
